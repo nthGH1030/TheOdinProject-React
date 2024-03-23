7970d6a264511d2234426e8c72547347002e9e27
Kelvin Ngan <nth1030@gmail.com>
2024-03-23 21:30:41 +0800

    2nd assignment part 9 commit

---
+++ 2nd-assignment-part9/src/App.js
@@ -0,0 +1,29 @@
+import { recipes } from './data.js';
+
+function Recipe({id, name, ingredients}){
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map(ingredient => 
+          <li key={ingredient}>
+            {ingredient}
+          </li>
+          )}
+      </ul>
+    </div>
+  );
+}
+
+export default function RecipeList() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map(recipe =>
+        <div key={recipe.id}>
+          <Recipe {...recipe} key = {recipe.id}/>
+        </div>
+      )}
+    </div>
+  );
+}
