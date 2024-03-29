234af24efc60c943d7d157b952a9ad614dbdfa36
Kelvin Ngan <nth1030@gmail.com>
2024-03-29 22:57:49 +0800

    first commit 4th-assignment-part4

---
+++ 4th-assignment-part4/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { letters } from './data.js';
+import Letter from './Letter.js';
+
+export default function MailClient() {
+  const [selectedIds, setSelectedIds] = useState([]);
+
+  // TODO: allow multiple selection
+  let selectedCount = selectedIds.length;
+
+  function handleToggle(toggledId) {
+    // if the if was selected, remove the id
+    if (selectedIds.includes(toggledId)){
+      setSelectedIds(selectedIds.filter(id =>
+        id !== toggledId));
+    } else {
+      //add the id to the array
+      setSelectedIds([
+        ...selectedIds,
+        toggledId
+      ]);
+    }
+  }
+
+  return (
+    <>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map(letter => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isSelected={
+              // TODO: allow multiple selection
+              selectedIds.includes(letter.id)
+            }
+            onToggle={handleToggle}
+          />
+        ))}
+        <hr />
+        <p>
+          <b>
+            You selected {selectedCount} letters
+          </b>
+        </p>
+      </ul>
+    </>
+  );
+}
